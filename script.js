cb4a556bb0d3834c22b673ec2996f4074e77effe
Copy <71971770+Copy05@users.noreply.github.com>
2023-06-22 11:24:20 +0200

    Optimisation

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-/**
- * Copyright Copy05 2023. All Rights Reserved
- */
-
 const copyr = document.getElementById('copyr')
 
 document.addEventListener("DOMContentLoaded", (e) => {
-    copyr.innerHTML = `Copyright 2021 – ${new Date().getFullYear().toString()} Copy05. All Rights Reserved`
+    copyr.innerHTML = `<span style="font-family: sans-serif;">&copy;</span> 2021 – ${new Date().getFullYear().toString()} Copy05. All Rights Reserved`
 })
