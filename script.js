f30f8299f5096d3d8689ca42c625b0bc08a19081
Copy <71971770+Copy05@users.noreply.github.com>
2023-07-13 11:47:42 +0200

    Stats

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ const copyr = document.getElementById('copyr')
 document.addEventListener("DOMContentLoaded", (e) => {
     copyr.innerHTML = `<span style="font-family: sans-serif;">&copy;</span> 2021 – ${new Date().getFullYear().toString()} Copy05. All Rights Reserved`
 })
+
+const counters = document.querySelectorAll('.numbergraph');
+const speed = 100;
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        const inc = target / speed;
+
+        if(count < target){
+            counter.innerText = Math.ceil(count + inc) 
+            setTimeout(updateCount, 1)
+        } else {
+            count.innerText = target;
+        }
+    }
+    updateCount();
+});
